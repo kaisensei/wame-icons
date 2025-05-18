64cd0a787480ef8adf62c5d784b450097615ba8e
jessechu.work <jessechu.work@gmail.com>
2025-05-18 02:58:39 +0000

    Add unit test for component Close

---
+++ src/common/Close.test.tsx
@@ -0,0 +1,75 @@
+import {expect, describe } from 'vitest';
+import { render } from '@testing-library/react';
+import Close from './Close';
+import { SvgSize } from '../util';
+
+describe('Close component', () => {
+  test('renders with default props', () => {
+    const { container } = render(<Close />);
+    const svgElement = container.querySelector('svg');
+    expect(svgElement).toBeInTheDocument();
+    expect(svgElement).toHaveAttribute('xmlns', 'http://www.w3.org/2000/svg');
+    expect(svgElement).toHaveAttribute('class', 'fill-gray-666');
+    expect(svgElement).toHaveAttribute('height', SvgSize.md.toString());
+    expect(svgElement).toHaveAttribute('width', SvgSize.md.toString());
+    expect(svgElement).toHaveAttribute('viewBox', '0 -960 960 960');
+    
+    const pathElement = document.querySelector('path');
+    expect(pathElement).toBeInTheDocument();
+    expect(pathElement).toHaveAttribute('d', 'm256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z')
+  });
+
+  test('renders with specified size', () => {
+    const customSize = SvgSize.lg;
+    render(<Close size={customSize} />);
+    
+    const svgElement = document.querySelector('svg');
+    expect(svgElement).toHaveAttribute('height', customSize.toString());
+    expect(svgElement).toHaveAttribute('width', customSize.toString());
+  });
+
+  test('renders with size of 0', () => {
+    const customSize = 0;
+    render(<Close size={customSize} />);
+    
+    const svgElement = document.querySelector('svg');
+    expect(svgElement).toHaveAttribute('height', customSize.toString());
+    expect(svgElement).toHaveAttribute('width', customSize.toString());
+  });
+
+  test('renders with negative size', () => {
+    const customSize = -1;
+    render(<Close size={customSize} />);
+    
+    const svgElement = document.querySelector('svg');
+    expect(svgElement).toHaveAttribute('height', customSize.toString());
+    expect(svgElement).toHaveAttribute('width', customSize.toString());
+  });
+
+  test('renders with negative decimal size', () => {
+    const customSize = -1.5;
+    render(<Close size={customSize} />);
+    
+    const svgElement = document.querySelector('svg');
+    expect(svgElement).toHaveAttribute('height', customSize.toString());
+    expect(svgElement).toHaveAttribute('width', customSize.toString());
+  });
+
+  test('renders with positive decimal size', () => {
+    const customSize = 1.5;
+    render(<Close size={customSize} />);
+    
+    const svgElement = document.querySelector('svg');
+    expect(svgElement).toHaveAttribute('height', customSize.toString());
+    expect(svgElement).toHaveAttribute('width', customSize.toString());
+  });
+
+  test('renders with specified className', () => {
+    const customClass = 'test-class';
+    render(<Close className={customClass} />);
+    
+    const svgElement = document.querySelector('svg');
+    expect(svgElement).toHaveClass(customClass);
+  });
+
+});
