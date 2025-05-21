4619b68f247ebc96114e594f61f94ac421f45f09
jessechu.work <jessechu.work@gmail.com>
2025-05-21 22:02:44 +0000

    Remove unnecessary testing cases.

--- src/common/Calendar.test.tsx
+++ src/common/Calendar.test.tsx
@@ -8,47 +8,12 @@ describe('Calendar component', () => {
     const { container } = render(<Calendar />);
     const svgElement = container.querySelector('svg');
     expect(svgElement).toBeInTheDocument();
-    expect(svgElement).toHaveAttribute('xmlns', 'http://www.w3.org/2000/svg');
-    expect(svgElement).toHaveAttribute('class', '');
     expect(svgElement).toHaveAttribute('height', SvgSize.md.toString());
     expect(svgElement).toHaveAttribute('width', SvgSize.md.toString());
-    expect(svgElement).toHaveAttribute('viewBox', '0 -960 960 960');
-    
-    const pathElement = document.querySelector('path');
-    expect(pathElement).toBeInTheDocument();
-    expect(pathElement).toHaveAttribute('d', 'M200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Zm280 240q-17 0-28.5-11.5T440-440q0-17 11.5-28.5T480-480q17 0 28.5 11.5T520-440q0 17-11.5 28.5T480-400Zm-160 0q-17 0-28.5-11.5T280-440q0-17 11.5-28.5T320-480q17 0 28.5 11.5T360-440q0 17-11.5 28.5T320-400Zm320 0q-17 0-28.5-11.5T600-440q0-17 11.5-28.5T640-480q17 0 28.5 11.5T680-440q0 17-11.5 28.5T640-400ZM480-240q-17 0-28.5-11.5T440-280q0-17 11.5-28.5T480-320q17 0 28.5 11.5T520-280q0 17-11.5 28.5T480-240Zm-160 0q-17 0-28.5-11.5T280-280q0-17 11.5-28.5T320-320q17 0 28.5 11.5T360-280q0 17-11.5 28.5T320-240Zm320 0q-17 0-28.5-11.5T600-280q0-17 11.5-28.5T640-320q17 0 28.5 11.5T680-280q0 17-11.5 28.5T640-240Z');
-
   });
 
   test('renders with specified size', () => {
     const customSize = SvgSize.lg;
-    render(<Calendar size={customSize} />);
-    
-    const svgElement = document.querySelector('svg');
-    expect(svgElement).toHaveAttribute('height', customSize.toString());
-    expect(svgElement).toHaveAttribute('width', customSize.toString());
-  });
-
-  test('renders with size of 0', () => {
-    const customSize = 0;
-    render(<Calendar size={customSize} />);
-    
-    const svgElement = document.querySelector('svg');
-    expect(svgElement).toHaveAttribute('height', customSize.toString());
-    expect(svgElement).toHaveAttribute('width', customSize.toString());
-  });
-
-  test('renders with negative size', () => {
-    const customSize = -1;
-    render(<Calendar size={customSize} />);
-    
-    const svgElement = document.querySelector('svg');
-    expect(svgElement).toHaveAttribute('height', customSize.toString());
-    expect(svgElement).toHaveAttribute('width', customSize.toString());
-  });
-
-  test('renders with negative decimal size', () => {
-    const customSize = -1.5;
     render(<Calendar size={customSize} />);
     
     const svgElement = document.querySelector('svg');
@@ -72,5 +37,4 @@ describe('Calendar component', () => {
     const svgElement = document.querySelector('svg');
     expect(svgElement).toHaveClass(customClass);
   });
-
 });
