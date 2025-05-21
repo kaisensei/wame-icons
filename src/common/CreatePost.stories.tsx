ead08c5cb74a9509019a0b15a854a2d9487f1563
jessechu.work <jessechu.work@gmail.com>
2025-05-21 18:44:26 +0000

    Add component CreatePost to storybook.

---
+++ src/common/CreatePost.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { expect } from '@storybook/test';
+import CreatePost from './CreatePost';
+import { SvgSize } from '../util';
+
+const meta: Meta<typeof CreatePost> = {
+  title: "common/CreatePost",
+  component: CreatePost,
+}; 
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {};
+
+export const blue: Story = {
+  name:"blue",
+  args:{
+    className: "fill-blue-500",
+  },
+  play: async ({ canvasElement }) => {
+    // Get the SVG element
+    const svg= canvasElement.querySelector('svg'); 
+
+    if (!svg) {
+      throw new Error('SVG element not found');
+    }
+    // Check if the SVG has the blue class
+    expect(svg.classList.contains('fill-blue-500')).toBe(true);
+  }
+}
+
+export const md: Story = {
+  name: "md",
+  args:{
+    className: '',
+    size: SvgSize.md,
+  },
+};
+
+export const lg: Story = {
+  name: "lg",
+  args:{
+    className: '',
+    size: SvgSize.lg,
+  },
+}
