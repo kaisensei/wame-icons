da123de2dac8cb83096249a7b4b3da9999d3953c
jessechu.work <jessechu.work@gmail.com>
2025-05-31 21:30:56 +0000

    Add unit test for component Info.

---
+++ src/common/Info.test.tsx
@@ -0,0 +1,40 @@
+import { expect, describe } from 'vitest';
+import { render } from '@testing-library/react';
+import Info from './Info';
+import { SvgSize } from '../util';
+
+describe('Info component', () => {
+  test('renders with default props', () => {
+    render(<Info />);
+    const svgElement = document.querySelector('svg');
+    expect(svgElement).toBeInTheDocument();
+    expect(svgElement).toHaveAttribute('height', SvgSize.md.toString());
+    expect(svgElement).toHaveAttribute('width', SvgSize.md.toString());
+  });
+
+  test('renders with specified size', () => {
+    const customSize = SvgSize.lg;
+    render(<Info size={customSize} />);
+    
+    const svgElement = document.querySelector('svg');
+    expect(svgElement).toHaveAttribute('height', customSize.toString());
+    expect(svgElement).toHaveAttribute('width', customSize.toString());
+  });
+
+  test('renders with positive decimal size', () => {
+    const customSize = 1.5;
+    render(<Info size={customSize} />);
+    
+    const svgElement = document.querySelector('svg');
+    expect(svgElement).toHaveAttribute('height', customSize.toString());
+    expect(svgElement).toHaveAttribute('width', customSize.toString());
+  });
+
+  test('renders with specified className', () => {
+    const customClass = 'test-class';
+    render(<Info className={customClass} />);
+    
+    const svgElement = document.querySelector('svg');
+    expect(svgElement).toHaveClass(customClass);
+  });
+});
