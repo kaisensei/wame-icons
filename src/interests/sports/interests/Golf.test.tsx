4acf5384f97a70ef998b78c70576a39eec5667f0
jessechu.work <jessechu.work@gmail.com>
2025-06-21 04:59:08 +0000

    Add unit test for component Golf.

---
+++ src/interests/sports/interests/Golf.test.tsx
@@ -0,0 +1,40 @@
+import { expect, describe } from 'vitest';
+import { render } from '@testing-library/react';
+import Golf from './Golf';
+import { SvgSize } from '../../../util';
+
+describe('Golf component', () => {
+  test('renders with default props', () => {
+    render(<Golf />);
+    const svgElement = document.querySelector('svg');
+    expect(svgElement).toBeInTheDocument();
+    expect(svgElement).toHaveAttribute('height', SvgSize.md.toString());
+    expect(svgElement).toHaveAttribute('width', SvgSize.md.toString());
+  });
+
+  test('renders with specified size', () => {
+    const customSize = SvgSize.lg;
+    render(<Golf size={customSize} />);
+    
+    const svgElement = document.querySelector('svg');
+    expect(svgElement).toHaveAttribute('height', customSize.toString());
+    expect(svgElement).toHaveAttribute('width', customSize.toString());
+  });
+
+  test('renders with positive decimal size', () => {
+    const customSize = 1.5;
+    render(<Golf size={customSize} />);
+    
+    const svgElement = document.querySelector('svg');
+    expect(svgElement).toHaveAttribute('height', customSize.toString());
+    expect(svgElement).toHaveAttribute('width', customSize.toString());
+  });
+
+  test('renders with specified className', () => {
+    const customClass = 'test-class';
+    render(<Golf className={customClass} />);
+    
+    const svgElement = document.querySelector('svg');
+    expect(svgElement).toHaveClass(customClass);
+  });
+});
